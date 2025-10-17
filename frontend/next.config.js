d777f02dd555a8674cfbd1e692596188824e5009
tuneshwarpyjamaa <amishharsoor3@outlook.com>
2025-10-17 23:43:18 +0530

    PWA

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  // Your existing Next.js config
+});
